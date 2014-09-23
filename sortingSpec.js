a242a08a1fb807ca8d3348817d8fd0d9a4da715d
Logle <vqlong2014@gmail.com>
2014-09-23 14:35:56 -0400

    finish the specs

---
+++ sortingSpec.js
@@ -0,0 +1,21 @@
+// writing tests for sorting
+
+describe("bubble_sort", function() {
+  it("sort an array using bubble method", function() {
+    expect(bubble_sort([6,5,4,3,2])).toEqual([2,3,4,5,6]);
+  });
+  it("works with an array of one element", function() {
+    expect(bubble_sort([6])).toEqual([6]);
+  });
+});
+
+
+describe("merge_sort", function() {
+  it("sort an array using merge method", function() {
+    expect(bubble_sort([6,5,4,3,2])).toEqual([2,3,4,5,6]);
+  });
+  it("works with an array of one element", function() {
+    expect(bubble_sort([6])).toEqual([6]);
+  });
+
+});
